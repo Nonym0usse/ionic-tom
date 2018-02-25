98faaea6ae162716f239da573c134995f68759d6
CYRIL VELLA <cvella@immo-one.fr>
2018-02-25 10:54:04 +0100

    sql : test 2

--- src/pages/ajout/ajout.ts
+++ src/pages/ajout/ajout.ts
@@ -3,7 +3,7 @@
 * @Date:   2018-01-19T22:18:19+01:00
 * @Email:  cyril.vella@yahoo.com
  * @Last modified by:   CYRIL VELLA
- * @Last modified time: 2018-02-25T10:48:52+01:00
+ * @Last modified time: 2018-02-25T10:53:48+01:00
 */
 
 import { Component } from '@angular/core';
@@ -43,7 +43,7 @@ export class AjoutPage {
   private createTable()
   {
     this.db.executeSql('CREATE TABLE IF NOT EXISTS `Boisson` ( `idBoisson` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `nom` TEXT NOT NULL, `prix` NUMERIC, `description` TEXT, `pompe1` INTEGER, `pompe2` INTEGER, `photo` TEXT, `actif` INTEGER NOT NULL, `EstAlcoolisee` INTEGER NOT NULL )', {})
-    .then(() => console.log('Executed SQL'))
+    .then(() => console.log('Table created'))
     .catch(e => console.log(e));
   }
 
@@ -64,9 +64,8 @@ export class AjoutPage {
     }
 
     this.db.executeSql('INSERT INTO `Boisson` (nom, prix, description, pompe1, pompe2, photo, actif, EstAlcoolisee) VALUES (\'' + this.boisson['nom'] + '\', '+ parseFloat(this.boisson['prix']) +', \''+ this.boisson['description'] +'\', '+ parseInt(this.boisson['pompe1']) +', '+ parseInt(this.boisson['pompe2']) +', \''+ this.boisson['photo'] +'\', '+ this.boisson['actif'] +', '+ this.boisson['EstAlcoolisee'] +', last_insert_rowid())', {})
-    .then(() => console.log('Movie inserted !'))
-    .then(() => console.log('Executed SQL'))
-    .catch(e => console.log(e));
+    .then(() => console.log('INSERT : OK'))
+    .catch(e => console.log("Error " + e));
   }
 
   displayBoisson()
@@ -74,18 +73,7 @@ export class AjoutPage {
     this.boisson2 = [];
     this.db.executeSql('SELECT name FROM `Boisson`', {})
     .then((data) => {
-
-      if(data == null) {
-        return;
-      }
-
-      if(data.rows) {
-        if(data.rows.length > 0) {
-          for(var i = 0; i < data.rows.length; i++) {
-            this.boisson2.push(data.rows.item(i).name);
-          }
-        }
-      }
+      console.log(data);
     });
   }
 }
